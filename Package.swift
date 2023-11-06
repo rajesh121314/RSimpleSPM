99cb02747f29dbfe1921b9bc31dc980cd50f19cd
Rajesh <rajesh.s@vrinsoft.com>
2023-11-06 19:31:14 +0530

    login view controler public access  specifier

--- Package.swift
+++ Package.swift
@@ -11,11 +11,18 @@ let package = Package(
             name: "RSimpleSPM",
             targets: ["RSimpleSPM"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "RSimpleSPM"),
+            name: "RSimpleSPM",
+            dependencies: ["SDWebImage"]
+            ),
         .testTarget(
             name: "RSimpleSPMTests",
             dependencies: ["RSimpleSPM"]),
---
+++ Sources/RSimpleSPM/Resources/RSSPMLoginVC.swift
@@ -0,0 +1,39 @@
+//
+//  RSSPMLoginVC.swift
+//  
+//
+//  Created by vrinsoft on 06/11/23.
+//
+
+import UIKit
+import SDWebImage
+
+public class RSSPMLoginVC: UIViewController {
+    
+    @IBOutlet weak var imgTopImage: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+        print("username \(emailTextField.text ?? "") password \(passwordTextField.text ?? "")")
+    }
+    
+    public init() {
+       // let bundle = Bundle(for: LoginVC.self)
+      //  let bundleId = bundle.bundleIdentifier
+        super.init(nibName: "LoginVC", bundle: Bundle.module)
+       // super.init(nibName: "LoginVC", bundle: Bundle(for: LoginVC.self))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        guard let url = URL(string: "https://picsum.photos/200/300") else { return  }
+        imgTopImage.sd_setImage(with: url)
+    }
+
+
+}
